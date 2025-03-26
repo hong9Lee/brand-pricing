84d0ac86d95d9ef4217c35e04170683cf82ca278
hong9Lee <hongtemp9mac@gmail.com>
2025-03-26 21:00:12 +0900

    chore: init

---
+++ src/main/kotlin/com/pjt/brandpricing/BrandpricingApplication.kt
@@ -0,0 +1,11 @@
+package com.pjt.brandpricing
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BrandpricingApplication
+
+fun main(args: Array<String>) {
+    runApplication<BrandpricingApplication>(*args)
+}
---
+++ src/test/kotlin/com/pjt/brandpricing/BrandpricingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pjt.brandpricing
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BrandpricingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
